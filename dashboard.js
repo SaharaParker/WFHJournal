a982ac937537664bd3db6dd2ee5f802c45078cd8
Sahara <THsahara@gmail.com>
2022-02-08 13:13:59 -0500

    created dashboard.js started adding some logic

---
+++ dashboard.js
@@ -0,0 +1,21 @@
+let entriesFromBackendDataStore = [
+    {
+        id: 1,
+        fullName: "Sahara Parker",
+        date: "2022-02-01",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum fugit in laudantium molestias nihil nostrum odio rem? Aperiam dolorum et iure nihil nostrum!"
+    }, {
+        id: 2,
+        fullName: "Mary Doe",
+        date: "2022-02-02",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa delectus facilis numquam!"
+    }, {
+        id: 3,
+        fullName: "Annete Doe",
+        date: "2022-02-03",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum est exercitationem laboriosam nostrum obcaecati omnis quibusdam reprehenderit saepe?"
+    }];
+
+const entryShow = document.getElementById('jourentries');
+
+entryShow(entriesFromBackendDataStore);
