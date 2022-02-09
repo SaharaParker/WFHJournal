bf1930cec459c367737991013e5bb318a3d07d83
Sahara <THsahara@gmail.com>
2022-02-09 00:16:20 -0500

    started checkIsNotEmpty function

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 const loginForm = document.getElementById('login');
 const errorDiv = document.getElementById('indexErr');
-const username = document.getElementById('username');
-const password = document.getElementById('password');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
 
 loginForm.addEventListener('submit',(event) =>{
     errorDiv.innerHTML = ""
     event.preventDefault();
+
+    if(
+        checkIsNotEmpty(usernameInput,"Username address is required")&&
+        checkIsNotEmpty(passwordInput,"Password is required.")
+    ){
+        window.location.href='/dashboard.html';
+
+    }
 });
+
+function checkIsNotEmpty(domInput,errorMessage){
+
+}
