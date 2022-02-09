6335c9d6d1daa2fc201a13e00e84f78c45f8f5f8
Sahara <THsahara@gmail.com>
2022-02-09 00:30:05 -0500

    added email const

--- signup.js
+++ signup.js
@@ -4,3 +4,6 @@ const firstInput = document.getElementById('firstName');
 const lastInput = document.getElementById('lastName');
 const newUsername = document.getElementById('newuser');
 const newPassword = document.getElementById('newpass');
+const emailInput = document.getElementById('email');
+
+
