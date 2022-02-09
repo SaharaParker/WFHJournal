1f8a6ef1483a3d19775a4a08cbada10357a457cd
Sahara <THsahara@gmail.com>
2022-02-09 00:28:11 -0500

    set consts!

---
+++ signup.js
@@ -0,0 +1,6 @@
+const signForm = document.getElementById('signup');
+const errDiv = document.getElementById('signErr');
+const firstInput = document.getElementById('firstName');
+const lastInput = document.getElementById('lastName');
+const newUsername = document.getElementById('newuser');
+const newPassword = document.getElementById('newpass');
