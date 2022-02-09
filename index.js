3f4266d2c9f2b094a589d203845d9c0c5e04d1a4
Sahara <THsahara@gmail.com>
2022-02-09 00:12:17 -0500

    added innerHTML and preventDefault

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const errorDiv = document.getElementById('indexErr');
 const username = document.getElementById('username');
 const password = document.getElementById('password');
 
+loginForm.addEventListener('submit',(event) =>{
+    errorDiv.innerHTML = ""
+    event.preventDefault();
+});
