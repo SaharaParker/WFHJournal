fcc3f026236965b7ffff1a5a91470295624e29ce
Sahara <THsahara@gmail.com>
2022-02-09 00:08:57 -0500

    in index.js did all getElementByIds

---
+++ index.js
@@ -0,0 +1,5 @@
+const loginForm = document.getElementById('login');
+const errorDiv = document.getElementById('indexErr');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+
