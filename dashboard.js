d51103a38827770fb474e4c5b4ea9782888a2b98
Sahara <THsahara@gmail.com>
2022-02-10 13:30:33 -0500

    filled displayed journal entries

---
+++ dashboard.js
@@ -0,0 +1,35 @@
+let journalEntries = [
+    {
+        staffId : 1,
+        firstName :"Sahara",
+        lastName: "Parker",
+        date: "2022-02-02",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, cumque, omnis.",
+        isPublic: false
+    },
+    {
+        staffId: 2,
+        firstName: "Naya",
+        lastName: "Nelson",
+        date: "2022-02-02",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing.",
+        isPublic: true
+    },
+    {
+        staffId: 3,
+        firstName: "Pete",
+        lastName: "Grey",
+        date: "2022-02-04",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur dignissimos dolorem eaque, expedita itaque libero magni modi qui sequi sint tenetur vitae voluptas.",
+        isPublic: true
+    },
+    {
+        staffId: 4,
+        firstName: "Greg",
+        lastName: "Lambert",
+        date: "2022-02-05",
+        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores doloribus eos modi necessitatibus omnis quidem, tempore ut!",
+        isPublic: false
+    },
+]
+
