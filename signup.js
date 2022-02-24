9be2e42a1731acb3d271b9938829954802435d17
Sahara <THsahara@gmail.com>
2022-02-24 18:19:09 -0500

    ran POST in API

--- signup.js
+++ signup.js
@@ -2,7 +2,7 @@ const firstNameValue = "Sahara"
 const lastNameValue = "Parker"
 const newpassValue = "Password1"
 const emailValue = "THsahara@WFH.com"
-const API_URL = "https://tkuelmfwvgrzbvyncons.supabase.co";
+const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`;
 const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDUxMzAzOTAsImV4cCI6MTk2MDcwNjM5MH0.mzpR2uu1eH7weegTdq4lthnsVpcfLN6pJe3zNnWI8W4";
 
 const signForm = document.getElementById('signup');
@@ -34,7 +34,6 @@ const signForm = document.getElementById('signup');
                 },
                 body: JSON.stringify(newUser)
                 })
-
             return signUser.json()
         }
     })
