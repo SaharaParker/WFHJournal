4a6815aac99984303e707bc7d26015cbffe6bdc7
Sahara <THsahara@gmail.com>
2022-02-24 13:26:12 -0500

    trying to change name of requirements

--- signup.js
+++ signup.js
@@ -5,32 +5,51 @@ const lastNameInput = document.getElementById('lastName').value;
 const newPassword = document.getElementById('newpass').value;
 const emailInput = document.getElementById('newemail').value;
 
+const firstName = "Sahara"
+const lastName = "Parker"
+const password = "Password1"
+const email = "THsahara@WFH.com"
 
-signForm.addEventListener('submit',(event) =>{
-    errDiv.innerHTML = ""
-    event.preventDefault();
+signForm.addEventListener('submit',(event) => {
+    event.preventDefault()
 
-    if(
-        checkIsNotEmpty(firstNameInput,"Enter Your First Name.")&&
-        checkIsNotEmpty(lastNameInput,"Enter Your Last Name.")&&
-        checkIsNotEmpty(newPassword,"Create a New Password.")&&
-        checkIsNotEmpty(emailInput,"Enter Your Email Address.")
-    ){
-        window.location.href = "/index.html";
+    newStaff(firstName,lastName,email,password)
+        .then(data => {
+            if(data === true) {
+                window.location.href = "/index.html";
+            }
+        })
 
+    async function newStaff(firstName,lastName,email,password) {
     }
-});
-
-function checkIsNotEmpty(domInput,errorMessage){
-    if (domInput.value === ""){
-        console.log(errorMessage);
-
-        const div = document.createElement('div');
-        div.innerHTML = `<p class="bg-gray-400 text-red-600 font-bold border-4">${errorMessage}</p>`;
-
-        errDiv.appendChild(div);
-
-     return false;
-    }
-    return true;
 }
+
+
+// signForm.addEventListener('submit',(event) =>{
+//     errDiv.innerHTML = ""
+//     event.preventDefault();
+//
+//     if(
+//         checkIsNotEmpty(firstNameInput,"Enter Your First Name.")&&
+//         checkIsNotEmpty(lastNameInput,"Enter Your Last Name.")&&
+//         checkIsNotEmpty(newPassword,"Create a New Password.")&&
+//         checkIsNotEmpty(emailInput,"Enter Your Email Address.")
+//     ){
+//         window.location.href = "/index.html";
+//
+//     }
+// });
+
+// function checkIsNotEmpty(domInput,errorMessage){
+//     if (domInput.value === ""){
+//         console.log(errorMessage);
+//
+//         const div = document.createElement('div');
+//         div.innerHTML = `<p class="bg-gray-400 text-red-600 font-bold border-4">${errorMessage}</p>`;
+//
+//         errDiv.appendChild(div);
+//
+//      return false;
+//     }
+//     return true;
+// }}
