64569ad882029aef505cb35f515a4e997eb3e10f
Sahara <THsahara@gmail.com>
2022-02-25 11:06:43 -0500

    tried to added first and last name consts

--- signup.js
+++ signup.js
@@ -6,6 +6,8 @@ const passwordNameEl = document.getElementById('newpass').value
 const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
 
+const firstName = "Sahara";
+const lastName = "Parker"
 const email = "THsahara@WFH.com";
 const password = "Password1";
 
@@ -13,15 +15,15 @@ const password = "Password1";
 signFormEl.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
-    signUp(email, password)
-        .then(data => {
-            if(data === true) {
+    signUp(firstName, lastName, email, password)
+        .then (data => {
+            if (data === true) {
                 window.location.href = 'index.html'
             }
         })
 })
 
 
-function signUp(email, password){
+function signUp(firstName, lastName, email, password){
 
 }
