2979c1e4f5e20ac3b195f9975ad728fb34aa34c4
Sahara <THsahara@gmail.com>
2022-02-25 01:11:59 -0500

    added api url and key

--- signup.js
+++ signup.js
@@ -2,8 +2,8 @@ const firstNameValue = document.getElementById('firstName').value
 const lastNameValue =  document.getElementById('lastName').value
 const newpassValue =  document.getElementById('newpass').value
 const emailValue =  document.getElementById('newemail').value
-const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co`
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw"
+const API_URL = 'https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup'
+const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
 const signForm = document.getElementById('signup')
 
 const emailValue = "THsahara@WFH.com";
