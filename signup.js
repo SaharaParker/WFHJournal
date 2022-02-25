795397d210101c025a9d87ac7439363751fabb9f
Sahara <THsahara@gmail.com>
2022-02-25 10:59:05 -0500

    if statement for data

--- signup.js
+++ signup.js
@@ -6,14 +6,19 @@ const passwordNameEl = document.getElementById('newpass').value
 const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
 
-const email = "THsahara@WFH.com"
-const password = "Password1"
+const email = "THsahara@WFH.com";
+const password = "Password1";
 
 
 signFormEl.addEventListener('submit', (evt) => {
     evt.preventDefault()
 
     signUp(email, password)
+        .then(data => {
+            if(data === true) {
+                window.location.href = 'index.html'
+            }
+        })
 })
 
 
