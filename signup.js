7adaedc7f2468079e70d1eb47cff8c871f23c915
Sahara <THsahara@gmail.com>
2022-02-25 13:23:23 -0500

    cleaned up signup.html and removed first and last name from signup.html and signup.js

--- signup.js
+++ signup.js
@@ -1,15 +1,8 @@
 const signFormEl = document.getElementById('signup')
-const firstNameEl = document.getElementById('firstName')
-const lastNameEl = document.getElementById('lastName')
 const emailNameEl = document.getElementById('newemail')
 const passwordNameEl = document.getElementById('newpass')
 const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
-
-// const firstName = "Sahara";
-// const lastName = "Parker"
-// const email = "thsahara@WFH.com";
-// const password = "Password1";
 
 
     signFormEl.addEventListener('submit', (evt) => {
@@ -45,6 +38,4 @@ const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
         const signedUpUser = await signUpUser.json()
         return signedUpUser
 
-        // console.log(signUpUser)
-        // return signUpUser.json()
     }
