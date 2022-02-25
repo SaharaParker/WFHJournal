76c86d15d7a27426e99ff75ea0f74fe1e36724ae
Sahara <THsahara@gmail.com>
2022-02-24 20:00:46 -0500

    add some consts

--- signup.js
+++ signup.js
@@ -1,23 +1,26 @@
-const firstNameValue = "Sahara"
-const lastNameValue = "Parker"
-const newpassValue = "Password1"
-const emailValue = "THsahara@WFH.com"
-const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`;
-const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw";
-const signForm = document.getElementById('signup');
+const firstNameValue = document.getElementById('firstName').value
+const lastNameValue =  document.getElementById('lastName').value
+const newpassValue =  document.getElementById('newpass').value
+const emailValue =  document.getElementById('newemail').value
+const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co`
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw"
+const signForm = document.getElementById('signup')
+
+const emailValue = "THsahara@WFH.com";
+const newpassValue = "Password1";
 
     signForm.addEventListener('submit',(event) => {
 
         event.preventDefault()
 
-        newStaff(firstNameValue, lastNameValue, emailValue, newpassValue)
+        newStaff(email,password)
             .then(data => {
                 if (data === true) {
                     window.location.href = "/index.html";
             }
     })
 
-    async function newStaff() {
+    async function newStaff(email, password) {
             const newUser = {
                 firstName: firstNameValue,
                 lastName: lastNameValue,
@@ -25,7 +28,7 @@ const signForm = document.getElementById('signup');
                 password: newpassValue
             }
 
-            const signUser = await fetch(`${API_URL}?apikey=${API_KEY}`,{
+            const signUser = await fetch(`${API_URL}/auth/v1/signup?apikey=${API_KEY}`,{
                 method:"POST",
                 headers: {
                     'Content-Type': 'application/json',
