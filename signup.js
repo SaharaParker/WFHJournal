6d6a1bbe8048aa3782588c9d608a0649453ddc44
Sahara <THsahara@gmail.com>
2022-02-25 10:48:35 -0500

    added consts for email and password. added event listener. started signup function

--- signup.js
+++ signup.js
@@ -1,7 +1,22 @@
-const signFormEl = document.getElementById('signup');
-const firstNameEl = document.getElementById('firstName');
-const lastNameEl = document.getElementById('lastName');
-const emailNameEl = document.getElementById('newemail');
-const passwordNameEl = document.getElementById('newpass');
-const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`;
+const signFormEl = document.getElementById('signup')
+const firstNameEl = document.getElementById('firstName').value
+const lastNameEl = document.getElementById('lastName').value
+const emailNameEl = document.getElementById('newemail').value
+const passwordNameEl = document.getElementById('newpass').value
+const API_URL = `https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
+
+const email = "THsahara@WFH.com"
+const password = "Password1"
+
+
+signFormEl.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+
+    signUp(email, password)
+})
+
+
+function signUp(email, password){
+
+}
