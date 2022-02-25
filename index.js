743adbdb4f266d64363eda2a18e6618854e9705b
Sahara <THsahara@gmail.com>
2022-02-25 13:30:47 -0500

    updated index.js

--- index.js
+++ index.js
@@ -7,8 +7,40 @@ const guestLogin = document.getElementById('submit-guest');
 const API_URL = 'https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/token?grant_type=password'
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
 
-// const email =
-// const password =
+loginFormEl.addEventListener('submit', (evt) => {
+    evt.preventDefault()
+    console.log(evt.target[2].value)
+    console.log(emailNameEl.value)
+    console.log(passwordNameEl.value)
+
+    logIn(emailNameEl.value, passwordNameEl.value)
+        .then (data => {
+            console.log(data)
+            if (data.email !== undefined) {
+                window.location.replace('/all-entries.html')
+            }
+        })
+})
+
+
+async function logIn(email, password) {
+    const existUser = {
+        email: email,
+        password: password
+    }
+
+    const logInUser = await fetch(`https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup`,{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw',
+        },
+        body: JSON.stringify(existUser)
+    })
+    const loggedInUser = await signUpUser.json()
+    return loggedInUser
+
+}
 
     loginForm.addEventListener('submit',(event) => {
         errorDiv.innerHTML = ""
@@ -23,11 +55,6 @@ const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsIn
         }
 
     });
-
-
-    // async function logIn(emailInput, passwordInput) {
-    //
-    // };
 
 function checkIsNotEmpty(domInput,errorMessage){
     if (domInput.value === ""){
