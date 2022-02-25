b2810c7b1d50ed7a9341faacc816fc11e6fb7798
Sahara <THsahara@gmail.com>
2022-02-25 03:59:58 -0500

    attempted another sign up auth

--- signup.js
+++ signup.js
@@ -1,42 +1,56 @@
-const firstNameValue = document.getElementById('firstName').value
-const lastNameValue =  document.getElementById('lastName').value
-const newpassValue =  document.getElementById('newpass').value
-const emailValue =  document.getElementById('newemail').value
+
 const API_URL = 'https://tkuelmfwvgrzbvyncons.supabase.co/auth/v1/signup'
 const API_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRrdWVsbWZ3dmdyemJ2eW5jb25zIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NTEzMDM5MCwiZXhwIjoxOTYwNzA2MzkwfQ.xO6WIhxzv4ngdiIVpz9DC9Wa0wOBrurvANuFsRjCfEw'
 const signForm = document.getElementById('signup')
-
-const emailValue = "THsahara@WFH.com";
-const newpassValue = "Password1";
 
     signForm.addEventListener('submit',(event) => {
 
         event.preventDefault()
 
-        signUp(email,password)
-            .then(data => {
-                if (data === true) {
-                    window.location.href = "/index.html";
-            }
-        });
+        const firstNameValue = event.target[0].value
+        const lastNameValue =  event.target[1].value
+        const emailValue = event.target[2].value
+        const newpassValue = event.target[3].value
 
+        console.log(emailValue)
+        console.log(newpassValue)
 
-        async function signUp(email,password){
-
-            const newStaff ={
-                email: email,
-                password: password
-            }
-
-            const signupUser = await fetch(`https://xvhrhlkmoaprvzmyyurp.supabase.co/auth/v1/signup`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2aHJobGttb2FwcnZ6bXl5dXJwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NDUwMjg4MCwiZXhwIjoxOTYwMDc4ODgwfQ._Nn8340qosB8uceqOqeVF4x-yeJiUz3MtXY30pDPeyY'
-                },
-                body: JSON.stringify(newUser)
+        let responsePromise = fetch (`${API_URL},`; {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'apikey': `${API_KEY}`,
+            },
+            body: JSON.stringify({
+                "email": emailValue,
+                "password": newpassValue
             })
+        })
+    })
 
-            return signUpUser.json()
-        }})
+        // async function signUp(email,password){
+        //
+        //     const newStaff ={
+        //         email: email,
+        //         password: password
+        //     }
+        //
+        //     const signupUser = await fetch(`https://xvhrhlkmoaprvzmyyurp.supabase.co/auth/v1/signup`, {
+        //         method: 'POST',
+        //         headers: {
+        //             'Content-Type': 'application/json',
+        //             'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh2aHJobGttb2FwcnZ6bXl5dXJwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY0NDUwMjg4MCwiZXhwIjoxOTYwMDc4ODgwfQ._Nn8340qosB8uceqOqeVF4x-yeJiUz3MtXY30pDPeyY'
+        //         },
+        //         body: JSON.stringify(newUser)
+        //     })
+        //
+        //     return signUpUser.json()
+        // }})
+
+// signUp(email,password)
+//     .then(data => {
+//         if (data === true) {
+//             window.location.href = "/index.html";
+//         }
+//     });
 
