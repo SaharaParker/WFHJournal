d60162672111d2f9983ff1a400caab873f5178c5
Sahara <THsahara@gmail.com>
2022-03-03 16:16:28 -0500

    added value to line 33

--- dashboard.js
+++ dashboard.js
@@ -31,7 +31,7 @@ fetch (`${API_URL}/rest/v1/entries?apikey=${API_KEY}`)
     .then(data => {
         const publicNews = data.filter(entries => entries.isPublic === true)
         const div = document.createElement('div')
-        div.innerHTML = `${publicNews}`
+        div.innerHTML = `${publicNews.value}`
         entryShow.appendChild(div)
         console.log("Public News Only")
         publicNews.map(entries => console.log(entries.firstName,entries.lastName,entries.created_at,entries.content))
