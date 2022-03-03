228e0baa34e4cde490a9a5443c64bcdbe4f58b6a
Sahara <THsahara@gmail.com>
2022-03-03 16:41:46 -0500

    trying to show whole entry

--- dashboard.js
+++ dashboard.js
@@ -31,7 +31,7 @@ fetch (`${API_URL}/rest/v1/entries?apikey=${API_KEY}`)
     .then(data => {
         const publicNews = data.filter(entries => entries.isPublic === true)
         console.log("Public News Only")
-        publicNews.map(entries => console.log(entries.firstName,entries.lastName,entries.created_at,entries.content))
+        publicNews.map(entries => console.log(entries))
         console.log(publicNews)
         console.log(publicNews[0].content)
         const div = document.createElement('div')
