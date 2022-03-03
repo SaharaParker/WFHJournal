a75a5a82c26458c7e9f6ee79476dc5e986d56dee
Sahara <THsahara@gmail.com>
2022-03-03 16:28:19 -0500

    added key

--- dashboard.js
+++ dashboard.js
@@ -34,7 +34,7 @@ fetch (`${API_URL}/rest/v1/entries?apikey=${API_KEY}`)
         publicNews.map(entries => console.log(entries.firstName,entries.lastName,entries.created_at,entries.content))
         console.log(publicNews)
         const div = document.createElement('div')
-        div.innerHTML = `${publicNews[key].content}`
+        div.innerHTML = `<p>${publicNews[key].content}</p>`
         entryShow.appendChild(div)
         return publicNews;
     });
